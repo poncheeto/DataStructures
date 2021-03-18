b021b7581f9d51e6a27fc1d241fc2f76455d3f5f
Poncho <jplyles1212@gmail.com>
2021-03-17 20:13:29 -0700

    Add remove node method

--- LinkedLists.py
+++ LinkedLists.py
@@ -36,9 +36,15 @@ class LinkedList:
     return string_list
   
   # Define your remove_node method below:
-  def remove_node(self,  value_to_remove):
+  def remove_node(self, value_to_remove):
     current_node = self.head_node
     if current_node.get_value() == value_to_remove:
       self.head_node = current_node.get_next_node()
       else:
-        
+        while current_node:
+            next_node = current_node.get_next_node()
+            if next_node.get_value() == value_to_remove:
+                current_node.set_next_node(next_node.get_next_node())
+                current_node = None
+            else:
+                curent_node = next_node
