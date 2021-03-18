089eaa819ff53caa78d302cc34541914a85f1c7f
Poncho <jplyles1212@gmail.com>
2021-03-17 20:49:37 -0700

    Correct spacing issue

--- LinkedLists.py
+++ LinkedLists.py
@@ -37,14 +37,14 @@ class LinkedList:
   
   # Define your remove_node method below:
   def remove_node(self, value_to_remove):
-    current_node = self.head_node
+    current_node = self.get_head_node()
     if current_node.get_value() == value_to_remove:
       self.head_node = current_node.get_next_node()
-      else:
-        while current_node:
-            next_node = current_node.get_next_node()
-            if next_node.get_value() == value_to_remove:
-                current_node.set_next_node(next_node.get_next_node())
-                current_node = None
-            else:
-                curent_node = next_node
+    else:
+      while current_node:
+        next_node = current_node.get_next_node()
+        if next_node.get_value() == value_to_remove:
+          current_node.set_next_node(next_node.get_next_node())
+          current_node = None
+        else:
+          current_node = next_node
