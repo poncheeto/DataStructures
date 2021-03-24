5520e49cfd19b4ad99d87a1471ccadbfc844d95f
Poncho <jplyles1212@gmail.com>
2021-03-23 21:56:40 -0700

    Add method to add node to head

--- DoublyLinkedLists.py
+++ DoublyLinkedLists.py
@@ -23,3 +23,7 @@ class DoublyLinkedList:
     def __init__(self):
         self.head_node = None
         self.tail_node = None
+
+    def add_to_head(self, new_value):
+        new_head = Node(new_value)
+        current_head = self.head_node 
