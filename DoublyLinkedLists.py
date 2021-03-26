45504c40e0ff9501eab45f0dd124c44a5ac9b37f
Poncho <jplyles1212@gmail.com>
2021-03-25 21:59:14 -0700

    Add spacing for readability

--- DoublyLinkedLists.py
+++ DoublyLinkedLists.py
@@ -27,19 +27,24 @@ class DoublyLinkedList:
     def add_to_head(self, new_value):
         new_head = Node(new_value)
         current_head = self.head_node
+
         if current_head != None:
             current_head.set_prev_node(new_head)
             new_head.set_next_node(current_head)
+
         self.head_node = new_head
+
         if self.tail_node == None:
             self.tail_node = head
 
     def add_to_tail(self, new_value):
         new_tail = Node(new_value)
         current_tail = self.tail_node
+
         if current_tail != None:
             current_tail.set_next_node(new_tail)
             new_tail.set_prev_node(current_tail)
         self.tail_node = new_tail
+        
         if self.head_node == None:
             self.head_node = new_tail
