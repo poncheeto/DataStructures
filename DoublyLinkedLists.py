4269c06150d9b95c8a6edff3ebd172fdb2181093
Poncho <jplyles1212@gmail.com>
2021-04-06 21:10:17 -0700

    Add stringify list method

--- DoublyLinkedLists.py
+++ DoublyLinkedLists.py
@@ -98,3 +98,12 @@ class DoublyLinkedList:
             next_node.set_prev_node(prev_node)
             prev_node.set_next_node(next_node)
         return node_to_remove
+
+        def stringify_list(self):
+            string_list = ""
+            current_node = self.head_node
+            while current_node:
+                if current_node.get_value() != None:
+                    string_list += str(current_node.get_value()) + "\n"
+                current_node = current_node.get_next_node()
+            return string_lists
