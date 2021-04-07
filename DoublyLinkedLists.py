c4634e5fcd0fec076a6512e6c2343e5cb1351fee
Poncho <jplyles1212@gmail.com>
2021-04-06 21:19:27 -0700

    Add items to tail of list and print to console

--- DoublyLinkedLists.py
+++ DoublyLinkedLists.py
@@ -113,3 +113,8 @@ class DoublyLinkedList:
     subway.add_to_head("Grand Central")
     subway.add_to_head("Central Park")
     print(subway.stringify_list())
+
+    subway.add_to_tail("Penn Station")
+    subway.add_to_tail("Wall Street")
+    subway.add_to_tail("Brooklyn Bridge")
+    print(subway.stringify_list())
